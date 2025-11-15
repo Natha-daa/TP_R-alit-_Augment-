782c5a4acbcc964d62b32da88ae9fe7603c8dea1
Natha-daa <nathanaeldaawe@gmail.com>
2025-11-15 15:19:32 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() {
